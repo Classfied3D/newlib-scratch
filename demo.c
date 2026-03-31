d757bfa67c1d1f0d94443584134163f7391b2a78
Classfied3D <82328013+Classfied3D@users.noreply.github.com>
2026-03-31 13:12:53 +0100

    Build to use newlib's ../configure (#1) (1/2)

--- demo.c
+++ demo.c
@@ -1,5 +1,10 @@
 #include <string.h>
+#include "sb3api.h"
 
 int main() {
-  return strlen("hello");
+  const char str[32];
+  while (!SB3_ask_str(str, "Enter a string: ", 32)) {}
+  int length = strlen(str);
+  SB3_say_dbl(length);
+  return 0;
 }
