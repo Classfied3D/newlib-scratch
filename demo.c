97725a1bb1079476aa91ed8a92af2cbe35a98e4b
Classfied3D <82328013+Classfied3D@users.noreply.github.com>
2026-05-08 18:00:31 +0100

    Update example with heap allocation

--- demo.c
+++ demo.c
@@ -8,11 +8,20 @@ int main() {
   int length = strlen(str);
   SB3_say_dbl(length);
   SB3_wait(0.3);
-  SB3_say_str("HI");
+  SB3_say_str("(fake) INITIALIZING...");
   SB3_wait(0.3);
 
-  void* lol = malloc(941);
-  SB3_say_dbl((long)lol);
-  free(lol);
+  void* allocations[5];
+  for (int i = 0; i < 5; i++) {
+    double size;
+    SB3_ask_dbl(&size, "How much to allocate?");
+    void* addr = malloc((long)size);
+    SB3_say_dbl((long)addr);
+    SB3_wait(0.5);
+  }
+
+  for (int i = 0; i < 5; i++) {
+    free(allocations[i]);
+  }
   return 0;
 }
